4ba584b3eb705ae31f21743465460c60601cc762
ignaciohita <ignaciohita@gmail.com>
2015-10-07 21:49:46 +0200

    Initial commit

--- www/js/main.js
+++ www/js/main.js
@@ -1,23 +1,32 @@
+/*global cordova*/
 var pluginListo = false,
     ultimaPosicion;
 
 function nuevaPosicionLista() {
     "use strict";
 
-    document.getElementById("posicionDispositivo").innerHTML = "Posición obtenida:<br> - Latitud: " + ultimaPosicion.coords.latitude + "<br> - Longitud: " + ultimaPosicion.coords.longitude;
+    document.getElementById("posicionDispositivo").innerHTML = "Posici&oacute;n obtenida:<br> - Latitud: " + ultimaPosicion.coords.latitude + "<br> - Longitud: " + ultimaPosicion.coords.longitude;
+    document.getElementById("botonMostrarPosicion").style.display = "block";
+}
+
+function mostrarPosicion() {
+    "use strict";
+
+    cordova.InAppBrowser.open("https://maps.google.com/?q=" + ultimaPosicion.coords.latitude + "," + ultimaPosicion.coords.longitude, "_blank");
 }
 
 function obtenerPosicion() {
     "use strict";
 
     if (pluginListo) {
-        document.getElementById("posicionDispositivo").innerHTML = "Obteniendo tu posición...";
+        document.getElementById("posicionDispositivo").innerHTML = "Obteniendo tu posici&oacute;n...";
 
         navigator.geolocation.getCurrentPosition(function (posicion) {
             ultimaPosicion = posicion;
             nuevaPosicionLista();
         }, function (error) {
-            document.getElementById("posicionDispositivo").innerHTML = "Error obteniendo posición:<br>" + error.code + " - " + error.message;
+            document.getElementById("posicionDispositivo").innerHTML = "Error obteniendo posici&oacute;n:<br>" + error.code + " - " + error.message;
+            document.getElementById("botonMostrarPosicion").style.display = "none";
         }, {
             maximumAge: 3000,
             timeout: 10000,
