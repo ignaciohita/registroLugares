8ece2afde21a3880fd7fa74784317c04baa6d2e7
ignaciohita <ignaciohita@gmail.com>
2015-10-12 20:13:51 +0200

    Initial commit

--- www/js/main.js
+++ www/js/main.js
@@ -1,7 +1,8 @@
 /*global cordova*/
 var pluginListo = false,
     ultimaPosicion,
-    referenciaNavegador;
+    referenciaNavegador,
+    baseDatos;
 
 function seleccionarTipoNavegador() {
     "use strict";
@@ -13,6 +14,7 @@ function nuevaPosicionLista() {
     "use strict";
 
     document.getElementById("posicionDispositivo").innerHTML = "Posici&oacute;n obtenida:<br> - Latitud: " + ultimaPosicion.coords.latitude + "<br> - Longitud: " + ultimaPosicion.coords.longitude;
+    document.getElementById("botonGuardarPosicion").style.display = "block";
 
     referenciaNavegador = cordova.InAppBrowser.open("https://maps.google.com/?q=" + ultimaPosicion.coords.latitude + "," + ultimaPosicion.coords.longitude, "_blank", "hidden=yes");
     referenciaNavegador.addEventListener("loadstop", function () {
@@ -42,6 +44,7 @@ function obtenerPosicion() {
         }, function (error) {
             document.getElementById("posicionDispositivo").innerHTML = "Error obteniendo posici&oacute;n:<br>" + error.code + " - " + error.message;
             document.getElementById("botonMostrarPosicion").style.display = "none";
+            document.getElementById("botonGuardarPosicion").style.display = "none";
             document.getElementById("labelCheckNavegador").style.display = "none";
         }, {
             maximumAge: 3000,
@@ -51,11 +54,101 @@ function obtenerPosicion() {
     }
 }
 
+function visualizarUltimaPosicionAgregada() {
+    "use strict";
+
+    var nodoNumero = document.createElement("li"),
+        fechaPosicion = new Date(ultimaPosicion.timestamp);
+    nodoNumero.appendChild(document.createTextNode(fechaPosicion.getDate() + "/" + (fechaPosicion.getMonth() + 1) + "/" + fechaPosicion.getFullYear() + " - " + fechaPosicion.getHours() + ":" + fechaPosicion.getMinutes() + ":" + fechaPosicion.getSeconds()));
+    document.getElementById("listaPosicionesGuardadas").appendChild(nodoNumero);
+}
+
+function recuperarPosicionesBaseDatos() {
+    "use strict";
+
+    var transaccion = baseDatos.transaction(["lugares"], "readonly"),
+        store = transaccion.objectStore("lugares"),
+        consulta = store.openCursor();
+
+    consulta.onsuccess = function (event) {
+        var posicion = event.target.result;
+
+        if (posicion) {
+            document.getElementById("tituloPosicionesGuardadas").style.display = "block";
+            ultimaPosicion = event.target.result.value;
+            visualizarUltimaPosicionAgregada();
+        }
+
+        posicion.continue();
+    };
+}
+
+function abrirBaseDatos() {
+    "use strict";
+
+    var nuevaBaseDatos = window.indexedDB.open("LugaresBaseDatos", 3);
+
+    nuevaBaseDatos.onupgradeneeded = function (event) {
+        //Este código sólo se ejecutará la primera vez
+
+        baseDatos = event.target.result;
+
+        if (!baseDatos.objectStoreNames.contains("firstOS")) {
+            baseDatos.createObjectStore("lugares", {
+                autoIncrement: true
+            });
+        }
+    };
+
+    nuevaBaseDatos.onsuccess = function (event) {
+        baseDatos = event.target.result;
+        recuperarPosicionesBaseDatos();
+    };
+}
+
+function agregarPosicionBaseDatos() {
+    "use strict";
+
+    document.getElementById("botonGuardarPosicion").style.display = "none";
+
+    var transaccion = baseDatos.transaction(["lugares"], "readwrite"),
+        store = transaccion.objectStore("lugares"),
+        operacion = store.add({
+            timestamp: ultimaPosicion.timestamp,
+            coords: {
+                latitude: ultimaPosicion.coords.latitude,
+                longitude: ultimaPosicion.coords.longitude,
+                altitude: ultimaPosicion.coords.altitude,
+                accuracy: ultimaPosicion.coords.accuracy,
+                altitudeAccuracy: ultimaPosicion.coords.altitudeAccuracy,
+                heading: ultimaPosicion.coords.heading,
+                speed: ultimaPosicion.coords.speed
+            }
+        });
+
+    operacion.onsuccess = function (e) {
+        visualizarUltimaPosicionAgregada();
+    };
+
+    operacion.onerror = function (e) {
+        document.getElementById("botonGuardarPosicion").style.display = "block";
+    };
+}
+
+function cerrarBaseDatos() {
+    "use strict";
+
+    baseDatos.close();
+}
+
 function dispositivoListo() {
     "use strict";
 
     pluginListo = true;
+
     document.getElementById("checkNavegador").checked = localStorage.getItem("preferenciaNavegadorExterno") === "true";
+
+    abrirBaseDatos();
 }
 
 function inicioAplicacion() {
